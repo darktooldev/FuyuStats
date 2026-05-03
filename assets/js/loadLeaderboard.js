16e66f73f1913970359e3d2c569c716e98f75f19
darktooldev <187148677+darktooldev@users.noreply.github.com>
2026-05-03 19:31:28 +0200

    fix leaderboard bug implemented new leaderboard algorithm. fixes bug that if 2 people have have spent the same amount, it is unknown how will appear first. now the first person that reached that amount will appear first.

--- assets/js/loadLeaderboard.js
+++ assets/js/loadLeaderboard.js
@@ -4,13 +4,11 @@ const USERNAME = 1;
 const COST = 3;
 
 const cacheJsonFilesMap = new Map();
-const map = new Map();
 
 document.getElementById("applyMonthsButton").addEventListener("click", fillLeaderboard);
 
 async function fillLeaderboard() {
     tableBody.innerHTML = `<tr><td colspan="3">Loading ...</td></tr>`;
-    map.clear();
 
     const files = [];
     for(let i = 0; i < selectedMonthsArray.length; i++) {
@@ -18,45 +16,48 @@ async function fillLeaderboard() {
             files.push(selectedMonthsArray[i].month+"-Data.json")
         }
     }
-
+    
     const allMonthsArray = await Promise.all(files.map(f => fetchJsonFileData(f)));
 
-    for(let i = 0; i < allMonthsArray.length; i++) {
-        let month = allMonthsArray[i];
-        for(let j = 0; j < month.length; j++) {
-            let redeem = month[j];
+    const userMap = new Map();
+    const userArray = [];
 
-            if(map.has(redeem[USERNAME])) {
-                let curr_amount = map.get(redeem[USERNAME]);
-                curr_amount += Number(redeem[COST]); 
-                map.set(redeem[USERNAME], curr_amount);
-            } else {
-                map.set(redeem[USERNAME], Number(redeem[COST]));
+    for (const month of allMonthsArray) {
+        for(const redeem of month) {
+            const username = redeem[USERNAME];
+            const cost = Number(redeem[COST]);
+
+            let index = userMap.get(username);
+
+            if(index === undefined) {
+                index = userArray.push([username, 0]);
+                index--;
             }
+            
+            userArray[index][1] += cost;
+            
+            while(index > 0 && userArray[index-1][1] < userArray[index][1]) {
+                let tempUser = userArray[index-1];
+                userArray[index-1] = userArray[index];
+                userArray[index] = tempUser;
+                
+                userMap.set(userArray[index][0], index);
+                index--;
+            }
+
+            userMap.set(username, index);
         }
     }
 
-    const arr = Array.from(map);
-
-    arr.sort((i1, i2) => i2[1] - i1[1]);
-
-    let rank = 1;
-
-    let rows = "";
-    arr.forEach(item => {
-        let text = `
-                    <tr>
-                        <td>${rank}</td>
-                        <td>${item[0]}</td>
-                        <td>${item[1]}</td>
-                    </tr>
-                    `;
-        rank++;
-
-        rows += text;
-    });
-
-    tableBody.innerHTML = rows;
+    tableBody.innerHTML = userArray.map((item, index) => {
+        return `
+        <tr>
+            <td>${index + 1}</td>
+            <td>${item[0]}</td>
+            <td>${item[1]}</td>
+        </tr>
+        `;
+    }).join('');
 }
 
 async function fetchJsonFileData(monthFileName) {
